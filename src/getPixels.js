0d87ecdc402070b28aca74e5820ae30ec80ec128
Lisa Müllner <92728067+muellnerlisa@users.noreply.github.com>
2026-02-06 15:04:45 +0100

    DVISA-3183: use cached HQ image data for pixeldata retrieval (#6)

--- src/getPixels.js
+++ src/getPixels.js
@@ -1,6 +1,7 @@
 import { getEnabledElement } from './enabledElements.js';
 import getStoredPixels from './getStoredPixels.js';
 import getModalityLUT from './internal/getModalityLUT.js';
+import { default as imageCache } from './imageCache.js';
 
 /**
  * Retrieves an array of pixels from a rectangular region with modality LUT transformation applied
@@ -12,10 +13,43 @@ import getModalityLUT from './internal/getModalityLUT.js';
  * @param {Number} height The height of the of the sampling rectangle in image coordinates
  * @returns {Array} The modality pixel value of the pixels in the sampling rectangle
  */
-export default function (element, x, y, width, height) {
-  const storedPixels = getStoredPixels(element, x, y, width, height);
+export default function (element, x, y, width, height, options = { hqImageId: undefined }) {
+  const storedPixels = getStoredPixels(element, x, y, width, height, options);
   const ee = getEnabledElement(element);
-  const mlutfn = getModalityLUT(ee.image.slope, ee.image.intercept, ee.viewport.modalityLUT);
+
+  const { slope, intercept } = getSlopeAndIntercept(ee, options.hqImageId);
+
+  const mlutfn = getModalityLUT(slope, intercept, ee.viewport.modalityLUT);
 
   return storedPixels.map(mlutfn);
+}
+
+
+/**
+ * Determines the slope and intercept values to use for modality LUT.
+ * If `hqImageId` is provided and a corresponding image is found loaded
+ * in the Cornerstone cache, returns its slope and intercept. Otherwise,
+ * falls back to the current enabled element's image values.
+ *
+ * @param {Object} enabledElement - Cornerstone enabled element with the current image.
+ * @param {String} [hqImageId] - Optional high-quality image ID to prefer for pixel data when available.
+ * @returns {{ slope: number, intercept: number }} Selected slope and intercept for LUT.
+ */
+function getSlopeAndIntercept (enabledElement, hqImageId) {
+  let { slope, intercept } = enabledElement.image;
+
+  if (hqImageId !== undefined) {
+    const foundHQImage = imageCache.imageCache[hqImageId];
+
+    if (foundHQImage && foundHQImage.loaded) {
+      slope = foundHQImage.image.slope;
+      intercept = foundHQImage.image.intercept;
+    }
+  }
+
+  return {
+    slope,
+    intercept
+  };
+
 }
--- test/getStoredPixels_test.js
+++ test/getStoredPixels_test.js
@@ -4,6 +4,7 @@ import enable from '../src/enable.js';
 import displayImage from '../src/displayImage.js';
 import getStoredPixels from '../src/getStoredPixels.js';
 import disable from '../src/disable.js';
+import { default as imageCache } from '../src/imageCache.js';
 
 describe('getStoredPixels', function () {
   beforeEach(function () {
@@ -56,6 +57,66 @@ describe('getStoredPixels', function () {
 
   it('should throw an error if the element is undefined', function () {
     assert.throws(() => getStoredPixels(undefined, 0, 0, 1, 1));
+  });
+
+  it('should use HQ image pixel data when available', function () {
+    // Arrange
+    const element = this.element;
+
+    // HQ image setup in cache
+    const hqImageId = 'hq-exampleImageId';
+    const hqGetPixelData = () => new Uint8Array([10, 11, 12, 13, 14, 15, 16, 17, 18]);
+    imageCache.imageCache[hqImageId] = {
+      image: {
+        getPixelData: hqGetPixelData
+      },
+      loaded: true
+    };
+
+    // Act
+    const storedPixelsHQ1 = getStoredPixels(element, 1, 1, 2, 2, { hqImageId });
+    const storedPixelsHQ2 = getStoredPixels(element, 0, 0, 1, 1, { hqImageId });
+    const storedPixelsHQ3 = getStoredPixels(element, 0, 1, 2, 2, { hqImageId });
+
+    // Assert (using HQ pixel data)
+    // From HQ pixelData:
+    // 10 11 12
+    // 13 14 15
+    // 16 17 18
+    assert.deepEqual(storedPixelsHQ1, [14, 15, 17, 18]);
+    assert.deepEqual(storedPixelsHQ2, [10]);
+    assert.deepEqual(storedPixelsHQ3, [13, 14, 16, 17]);
+
+    // Cleanup HQ image from cache
+    delete imageCache.imageCache[hqImageId];
+  });
+
+  it('should fall back when HQ image is cached but not loaded', function () {
+    // Arrange
+    const element = this.element;
+
+    // HQ image present in cache but not loaded
+    const hqImageId = 'hq-exampleImageId-not-loaded';
+    const hqGetPixelData = () => new Uint8Array([100, 101, 102, 103, 104, 105, 106, 107, 108]);
+    imageCache.imageCache[hqImageId] = {
+      image: {
+        getPixelData: hqGetPixelData
+      },
+      loaded: false
+    };
+
+    // Act: should use the base image pixel data, not HQ
+    const storedPixels1 = getStoredPixels(element, 1, 1, 2, 2, { hqImageId });
+    const storedPixels2 = getStoredPixels(element, 0, 0, 1, 1, { hqImageId });
+    const storedPixels3 = getStoredPixels(element, 0, 1, 2, 2, { hqImageId });
+
+    // Assert: values match base image [1..9]
+    assert.deepEqual(storedPixels1, [5, 6, 8, 9]);
+    assert.deepEqual(storedPixels2, [1]);
+    assert.deepEqual(storedPixels3, [4, 5, 7, 8]);
+
+    // Cleanup
+    delete imageCache.imageCache[hqImageId];
   });
 
   afterEach(function () {
